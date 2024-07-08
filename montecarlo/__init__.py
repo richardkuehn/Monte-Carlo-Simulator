6a8af090def5251009a69b95fc4ddecd82cc433c
Richard 'Ricky' Kuehn <richardprinzkuehn@gmail.com>
2024-07-08 15:33:53 -0400

    FP3

---
+++ montecarlo/__init__.py
@@ -0,0 +1,5 @@
+from montecarlo.montecarlo import Die, Game, Analyzer
+import pandas as pd
+import numpy as np
+
+print("I've imported the classes 'Die', 'Game' and 'Analyzer' from the montecarlo module!")
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'montecarlo',
+    version = '0.1',
+    author = 'Richard "Ricky" Kuehn',
+    author_email = 'fmt2tg@virginia.edu',
+    packages = find_packages(),
+    url = 'https://github.com/richardkuehn/DS5100-finalproject-fmt2tg',
+    license = open('LICENSE').read()
+    description = "package to keep track of readers' name, email, favorite genre, books read, and rating",
+    long_description = open('README.md').read(),
+    install_requires = "Pandas >= 2.2.2", "Numpy"
+
+    )
